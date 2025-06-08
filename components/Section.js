dfb45de6175bd02217286d6ffb300560a1fbd69c
bfinger30 <bfinger30@gmail.com>
2025-06-08 13:55:28 -0400

    Section.js Implemented + refactor

---
+++ components/Section.js
@@ -0,0 +1,18 @@
+export default class Section {
+  constructor({ items, renderer, containerSelector }) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  renderItems() {
+    this._items.forEach((item) => {
+      const todoElement = this._renderer(item);
+      this.addItem(todoElement);
+    });
+  }
+
+  addItem(element) {
+    this._container.append(element);
+  }
+}
